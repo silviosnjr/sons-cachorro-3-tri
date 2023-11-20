3e206830f4e743eafc1a37f43eb4d4041908a26a
silviosnjr <silviosnjr@gmail.com>
2023-11-20 16:56:52 -0300

    ajustes

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
 function tocaSom(idelementoaudio){
-     document.querySelector("#som_tecla_bolinha").play();
+     document.querySelector(idelementoaudio).play();
 }
-  const ListadeTeclas = document.querySelectorAll(".tecla");
-  let contador = 0; 
+const ListadeTeclas = document.querySelectorAll(".tecla");
+let contador = 0; 
 
  while (contador < ListadeTeclas.length) {
-
+    const efeito = ListadeTeclas[contador].classList[1]; 
+    const idAudio = "#som_"+efeito; 
+    ListadeTeclas[contador].onclick = function (){
+      tocaSom(idAudio)
+    }
+    contador = contador + 1;
+    console.log(contador);
  }
-   const efeito = ListadeTeclas[contador].classList[1]; 
-   const idAudio = "som_"+efeito; 
-   ListadeTeclas[contador].onclik = function
-   tocaSom(idAudio) {
-     
-   }
-   ListadeTeclas[contador].onclik = tocaSomBolinha;
-     contador = contador + 1;
-     console.log(contador);{
- }
-
-  
-
-
